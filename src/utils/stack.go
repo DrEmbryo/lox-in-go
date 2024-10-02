e56ca83fc75daa4ef2ec89e62f662bcea469e8e2
DrEmbryo <p.i.goroshko@gmail.com>
2024-10-02 11:30:29 -0700

    add StackError

--- src/utils/stack.go
+++ src/utils/stack.go
@@ -1,6 +1,20 @@
 package utils
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type StackError struct {
+	Message string
+}
+
+func (e StackError) Print() {
+	fmt.Println(e.Message)
+}
+
+func (e StackError) Error() string {
+	return e.Message
+}
 
 type Stack[T any] struct {
 	items []T
@@ -23,7 +37,7 @@ func (s *Stack[T]) IsEmpty() bool {
 
 func (s *Stack[T]) Peek() (T, error) {
 	if s.IsEmpty() {
-		return s.items[0], fmt.Errorf("stack is empty")
+		return *new(T), StackError{Message: "Stack is empty"}
 	}
 	return s.items[len(s.items)-1], nil
 }
@@ -34,7 +48,7 @@ func (s *Stack[T]) Len() int {
 
 func (s *Stack[T]) Get(i int) (T, error) {
 	if s.IsEmpty() {
-		return s.items[0], fmt.Errorf("stack is empty")
+		return *new(T), StackError{Message: "Stack is empty"}
 	}
 	return s.items[i], nil
 }
